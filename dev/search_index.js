807cc0e214921eb8ac25f7e1c93ec6aef9d58fe3
Documenter.jl <documenter@juliadocs.github.io>
2021-02-11 13:54:44 +0000

    build based on b0175c1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NetworksCourse","category":"page"},{"location":"#NetworksCourse","page":"Home","title":"NetworksCourse","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NetworksCourse]","category":"page"}]
+}
